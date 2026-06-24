a651924b1c6134dff133b9d4c147f8457712c76a
hirokazutoki <hrkztk1124@gmail.com>
2026-06-25 00:04:37 +0900

    add: structured data

--- astro/src/i18n/translations.ts
+++ astro/src/i18n/translations.ts
@@ -2,10 +2,16 @@ export const translations = {
   en: {
     name: 'Hirokazu Toki',
     role: 'Web Application Developer',
+    givenName: 'Hirokazu',
+    familyName: 'Toki',
+    alternateName: ['時 大和', 'とき ひろかず'],
   },
   ja: {
     name: '時 大和',
+    givenName: '大和',
+    familyName: '時',
     role: 'Webアプリケーションエンジニア',
+    alternateName: ['とき ひろかず', 'Hirokazu Toki'],
   },
 } as const;
 
